a9c7e61ae5d66e0de5aa5cc3ccd45a52baf08c7a
diego1019 <78331094+diego1019@users.noreply.github.com>
2021-02-01 10:46:21 +0800

    Add files via upload

---
+++ D_2.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb  1 10:02:20 2021
+
+@author: MTAEXAM
+"""
+
+top=float(input("上底:"))
+bottom=float(input("下底:"))
+height=float(input("高:"))
+area=(top+bottom)/2*height
+print("梯形面積:"+str(area))
