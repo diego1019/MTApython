224d103d284a9536dd9c2807e83bdd3c3dd3423c
diego1019 <78331094+diego1019@users.noreply.github.com>
2021-02-01 17:05:15 +0800

    Add files via upload

---
+++ D1-4.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb  1 15:12:47 2021
+
+@author: TQC
+"""
+
+n=1
+total=0
+while n<=10:
+    total+=n
+print(total)
---
+++ D1-5.py
@@ -0,0 +1,12 @@
+100# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb  1 15:35:52 2021
+
+@author: TQC
+"""
+
+total=1
+n=int(input("階:"))
+for n in range(1,n+1):
+    total*=n
+    print(total)
